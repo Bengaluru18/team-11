c89ff201101f98b7d250885e07814d53546fddca
Jess2110 <33127242+Jess2110@users.noreply.github.com>
2018-07-07 20:21:07 +0530

    Connection establishment

---
+++ connect.php
@@ -0,0 +1,42 @@
+<?php
+$username=filter_input(INPUT_POST,'username');
+$password=filter_input(INPUT_POST,'password');
+if(!empty($username)){
+    if(!empty($password)){
+$host="13.250.26.71";
+$dbusername="abc";
+$dbpass="123";
+$dbname="sskdb";
+//Create the connection
+$conn=new mysqli($host,$dbusername,$dbpass,$dbname);
+if(mysqli_connect_error()){
+echo "error";
+
+}
+else{
+echo "Hello there";
+$sql="INSERT INTO account(username,password) values('$username','$password')";
+if($conn->query($sql)){
+echo "New record is inserted successfully";
+}
+else{
+    echo "Error:".$sql."<br>".$conn->error;
+}
+$conn->close();
+}
+
+
+
+    }
+    else{
+    echo " Password should not be empty";
+    die();
+
+    }
+}
+else{
+    echo "Username should not be empty";
+    die();
+}
+
+?>
